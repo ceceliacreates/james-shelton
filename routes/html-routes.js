6fddd1546dc03a86cfd1d86acee2db9ab5b67889
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-10-11 22:52:35 -0400

    removes route for about page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,10 +5,6 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 
-    app.get("/about", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/about.html"));
-    })
-
     app.get("/figures", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/figures.html"));
     })
